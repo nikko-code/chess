12a731cfaae40f90018488c77ef8e64966b99169
Nikko Nguyen <nikkonguyenca@gmail.com>
2024-11-17 17:31:43 -0800

    Finish King, finish knight

--- src/piece/Knight.java
+++ src/piece/Knight.java
@@ -15,5 +15,19 @@ public class Knight extends Piece{
 			
 		}
 	}
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol,targetRow)) {
+			// knight can move if its movement ratio of col and row is 1:2 or 2:1
+			if(Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+				if(isValidSquare(targetCol,targetRow)) {
+					return true;
+				}
+			}
+			
+			
+		}
+		return false;
+	}
 
 }
