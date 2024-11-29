515f8cde0397613b9d0f123c9625f014f7402f69
Nikko Nguyen <nikkonguyenca@gmail.com>
2024-11-28 17:14:37 -0800

    Finish Knight, Rook, and Queen

--- src/piece/Bishop.java
+++ src/piece/Bishop.java
@@ -15,5 +15,19 @@ public class Bishop extends Piece{
 			
 		}
 	}
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard (targetCol,targetRow) && isSameSquare(targetCol,targetRow) == false) {
+			
+			if(Math.abs(targetCol - preCol) == Math.abs(targetRow - preRow)) {
+				if(isValidSquare(targetCol,targetRow) && pieceIsOnDiagonalLine(targetCol,targetRow) == false) {
+					return true;
+				}
+				
+			}
+			
+		}
+		return false;
+	}
 
 }
--- src/piece/Queen.java
+++ src/piece/Queen.java
@@ -15,5 +15,25 @@ public class Queen extends Piece{
 			
 		}
 	}
-
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol,targetRow) && isSameSquare(targetCol,targetRow) == false) {
+			
+			// Vertical & Horizontal
+			if(targetCol == preCol || targetRow == preRow) {
+				if(isValidSquare(targetCol,targetRow) && pieceIsOnStraightLine(targetCol,targetRow) == false) {
+					return true;
+					
+				}
+			}
+		}
+		
+		// Diagonal
+		if(Math.abs(targetCol - preCol) == Math.abs(targetRow - preRow)) {
+			if(isValidSquare(targetCol,targetRow) && pieceIsOnDiagonalLine(targetCol,targetRow) == false) {
+				return true;	
+			}
+		}
+		return false; 	
+	}
 }
